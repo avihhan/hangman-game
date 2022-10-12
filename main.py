90aafd2cdd9e8c6f8b4b3ab4340117f12cbcc208
avihhan <109458161+avihhan@users.noreply.github.com>
2022-10-11 23:04:57 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#final copy
-#hangman
+# final copy
+# hangman
 
-import random
+from random_word import RandomWords
+
 
 stages = ['''
   +---+
@@ -60,16 +61,29 @@ stages = ['''
 =========
 ''']
 
+hang_man_graphic = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                                   '''
 
+# word_list = ["aardvark", "baboon", "camel"]
 
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+#using pip random word generator to get random words
+
+r = RandomWords()
+chosen_word = r.get_random_word()
 word_length = len(chosen_word)
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# Testing code
+print(hang_man_graphic+"\n\n")
+print(f'The solution is {chosen_word}.') # cheat code
 
-#Create blanks
+# Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
@@ -78,17 +92,29 @@ end_of_game = False
 
 player_lives = 6
 
+guessed_letters = []
+
 while not end_of_game:
-  
+
+    pos_found = False
+
     guess = input("Guess a letter: ").lower()
 
-    #Check guessed letter
-  
-    pos_found = False
-  
+    guessed_letter_found = False
+    for letter in guessed_letters:
+        if guess == letter:
+            guessed_letter_found = True
+
+    if guessed_letter_found:
+        print(f"You have already guessed '{guess}' before. Try a different letter.")
+        continue
+
+    guessed_letters.append(guess)
+    # Check guessed letter
+
     for position in range(word_length):
         letter = chosen_word[position]
-        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
             pos_found = True
@@ -96,11 +122,11 @@ while not end_of_game:
     print(display)
 
     if not pos_found:
-      player_lives-=1
-      print(f"You have {player_lives} tries left.\n")
-      print(stages[player_lives])
-      
-    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+        player_lives -= 1
+        print(f"You have {player_lives} tries left.\n")
+        print(stages[player_lives])
+
+    # Check if there are no more "_" left in 'display'. Then all letters have been guessed.
     if "_" not in display:
         end_of_game = True
         print("You win.")
@@ -108,5 +134,3 @@ while not end_of_game:
     if player_lives == 0:
         end_of_game = True
         print("You loose.")
-
-  
