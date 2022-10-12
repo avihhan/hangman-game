facdcb31500c47ef1e1213d8b3db428938f0747c
avihhan <109458161+avihhan@users.noreply.github.com>
2022-10-11 22:34:12 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,112 @@
+#Step 4
+
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_length):
+    display += "_"
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+end_of_game = False
+
+player_lives = 6
+
+while not end_of_game:
+  
+    guess = input("Guess a letter: ").lower()
+
+    #Check guessed letter
+  
+    pos_found = False
+  
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+            pos_found = True
+
+    print(display)
+
+    if not pos_found:
+      player_lives-=1
+      print(f"You have {player_lives} tries left.\n")
+      print(stages[player_lives])
+      
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    if player_lives == 0:
+        end_of_game = True
+        print("You loose.")
+
+  
